d4a785d9aaed2b89d0d79f77cfe8c9643861aa00
Dat Thai <dat_thai@datahouse.com>
2025-01-02 00:15:13 +0700

    chore: add comment to api auth

--- src/services/auth/authService.ts
+++ src/services/auth/authService.ts
@@ -2,7 +2,9 @@ import { apiService } from '../apiService';
 import { LoginBodyRequest, RegisterBodyRequest } from './auth.model';
 
 export const authService = {
+  // The register function sends a POST request to the /auth/register endpoint with the data provided in the body.
   register: (data: RegisterBodyRequest) =>
     apiService.post('/auth/register', data),
+  // The login function sends a POST request to the /auth/login endpoint with the data provided in the body.
   login: (data: LoginBodyRequest) => apiService.post('/auth/login', data),
 };
